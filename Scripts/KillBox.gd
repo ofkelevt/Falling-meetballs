afeab203ae27d7eb2fa525594b0b4c5ce40f824b
adidee2006 <67602804+adidee2006@users.noreply.github.com>
2025-09-26 17:22:13 +0300

    killbox hopefully

---
+++ Scripts/KillBox.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+@export var respawn_position: Vector3 = Vector3(8, 9, -3)
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node3D) -> void:
+	# Compare the node's name
+	if body.name == "Player":
+		body.global_position = respawn_position
+	else:
+		body.queue_free()
