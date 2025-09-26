9746192048159410da9d92345021c0f5c61a5a5a
ofek levy <ievilootedo@outlook.com>
2025-09-26 15:59:45 +0300

    basic

---
+++ Spawner.cs
@@ -0,0 +1,30 @@
+extends Node3D   # use Node2D if 2D
+
+@export var spawned_scene: PackedScene
+@export var spawn_rate: float = 1.0
+@export var positions: Array[Vector3] = []
+@export var relative_position: bool = true
+@export var spawn_time_variance: float = 0.0
+
+var current_time: float = 0.0
+
+func _ready():
+	# Spawn initial objects
+	for pos in positions:
+		var spawn_pos = pos + (relative_position ? global_transform.origin : Vector3.ZERO)
+		var obj = spawned_scene.instantiate()
+		obj.global_transform.origin = spawn_pos
+		add_child(obj)
+
+func _process(delta: float):
+	current_time += delta
+	if current_time >= spawn_rate:
+		current_time -= spawn_rate
+		spawn()
+		if spawn_time_variance > 0.0:
+			current_time += randf_range(0.0, spawn_time_variance)
+
+func spawn():
+	var obj = spawned_scene.instantiate()
+	obj.global_transform.origin = global_transform.origin
+	add_child(obj)
